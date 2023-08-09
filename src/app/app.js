9e26bcee6a2e755a924914ecbbf96ccd7038ce8b
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-08-09 01:33:01 +0000

    ADD cors

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require("cors");
 
 const routeIndex = require('../route/routeIndex.js');
 const userRoutes = require ('../route/userRoute.js');
 const authRoutes = require ('../route/authRoute.js')
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(routeIndex);
 app.use(userRoutes);
