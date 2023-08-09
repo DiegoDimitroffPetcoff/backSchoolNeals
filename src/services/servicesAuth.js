8c10b6f59161fb5ce06e49e7de76708a90b7ddbc
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-08-09 21:35:59 +0100

    FIX AUTH

--- src/services/servicesAuth.js
+++ src/services/servicesAuth.js
@@ -3,18 +3,14 @@ const mongoose = require("mongoose");
 
 
 getByIdUsers = async (data) => {
-
-;
-
 const { nickname, password } = data;
-console.log(nickname);
-console.log(password)
+
 try {
   const user = await User.findOne({ nickname, password });
   if (user) {
-    res.status(200).json(user);
+    return user;
   } else {
-    res.status(404).json({ message: "Usuario no encontrado" });
+return null
   }
 } catch (error) {
   res.status(500).json({ message: "Error del servidor" });
