2a004839dbff3dee7c6e8d1ab1299a564d9d52dd
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-08-13 18:08:34 +0000

    ADD logs headers

--- src/middleWare/checkAuth.js
+++ src/middleWare/checkAuth.js
@@ -3,11 +3,12 @@ const { verifyToken } = require("../utils/handleJwt");
 const checkAuth = async (req, res, next) => {
   try {
     const cookieHeader = req.headers.cookie;
+    console.log(cookieHeader);
     const token = JSON.parse(
       decodeURIComponent(cookieHeader.split("userData=")[1])
     );
     const tokenData = verifyToken(token.tokenSession);
-    console.log(tokenData);
+
     if (tokenData) next();
   } catch (error) {
     res.status(405);
