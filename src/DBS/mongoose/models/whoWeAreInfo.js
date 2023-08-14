160c64854bb5efd22141b3f2ebc751c8903b4c83
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-08-14 19:52:21 +0000

    FIX text model

--- src/DBS/mongoose/models/whoWeAreInfo.js
+++ src/DBS/mongoose/models/whoWeAreInfo.js
@@ -4,7 +4,7 @@ const WhoWeAreSchemma = mongoose.Schema({
 
   title: { type: String },
   subTitle: {    type: String  },
- text:{tupe:String}
+ text:{type:String}
 });
 
 module.exports = mongoose.model("WhoWeAre", WhoWeAreSchemma);
